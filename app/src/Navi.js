fd4829697c2ee0fce79d8873288188eb0828489f
Priit Kallas <kallaspriit@gmail.com>
2013-01-08 16:25:28 +0200

    Started on navi stack.

--- app/src/Navi.js
+++ app/src/Navi.js
@@ -12,7 +12,7 @@ function(Bindable, dbg, util, resourceManager, angular) {
 	 * @module Core
 	 */
 	var Navi = function() {
-		this._activeModule = null;
+		this._stack = [];
 	};
 
 	Navi.prototype = new Bindable();
@@ -34,16 +34,17 @@ function(Bindable, dbg, util, resourceManager, angular) {
 	 * @param {String} module Module to open
 	 * @param {String} [action=index] Action to navigate to
 	 * @param {Array} [parameters=null] Action parameters
-	 * @param {Function} [readyCallback=null] Optional callback to call when ready
+	 * @param {Function} [doneCallback=null] Optional callback to call when ready
 	 * @return {Navi} Self
 	 */
-	Navi.prototype.open = function(module, action, parameters, readyCallback) {
+	Navi.prototype.open = function(module, action, parameters, doneCallback) {
 		action = action || 'index';
 		parameters = parameters || [];
 
 		dbg.log('! Opening module' + module + '::' + action + ' ' + util.str(parameters));
 
-		var className = util.convertEntityName(module) + 'Module',
+		var self = this,
+			className = util.convertEntityName(module) + 'Module',
 			actionName = util.convertCallableName(action) + 'Action';
 
 		window.app = window.app || {};
@@ -51,7 +52,7 @@ function(Bindable, dbg, util, resourceManager, angular) {
 
 		resourceManager.loadModule(module, function(moduleObj) {
 			resourceManager.loadView(module, action, function(viewContent) {
-				console.log('navi loaded', moduleObj, viewContent);
+				self._appendNavigation(module, action, parameters);
 
 				window.app.modules[className] = moduleObj;
 
@@ -63,13 +64,11 @@ function(Bindable, dbg, util, resourceManager, angular) {
 
 				angular.bootstrap(contentWrap, ['app']);
 
-				if (util.typeOf(readyCallback) === 'function') {
-					readyCallback(module, action, parameters, moduleObj);
+				if (util.typeOf(doneCallback) === 'function') {
+					doneCallback(module, action, parameters, moduleObj);
 				}
 			});
 		});
-
-		this._activeModule = module;
 
 		return this;
 	};
@@ -82,6 +81,68 @@ function(Bindable, dbg, util, resourceManager, angular) {
 	 */
 	Navi.prototype.getActiveModuleName = function() {
 		return this._activeModule;
+	};
+
+	/**
+	 * Append an item to the navigation stack.
+	 *
+	 * @param {String} module Name of the module to use
+	 * @param {String} action Module action to call, defaults to index
+	 * @param {Object} parameters Map of parameters to pass to action
+	 */
+	Navi.prototype._appendNavigation = function(module, action, parameters) {
+		var duplicate = false,
+			allowDuplicate = util.typeOf(parameters._allowDuplicate) === 'undefined'
+				? false
+				: parameters._allowDuplicate,
+			last;
+
+		if (!allowDuplicate && this._stack.length > 0) {
+			last = this._stack[this._stack.length - 1];
+
+			if (last.module == module && last.action == action) {
+				duplicate = true;
+			}
+		}
+
+		if (duplicate) {
+			this._stack.pop();
+		}
+
+		this._stack.push({
+			module: module,
+			action: action,
+			parameters: parameters
+		});
+
+		if (
+			(
+				util.typeOf(parameters._allowLoops) === 'undefined'
+				|| parameters._allowLoops != true
+			)
+			&& !allowDuplicate
+		) {
+			var lastItem = this._stack[this._stack.length - 1],
+				lastModule = lastItem.module,
+				lastAction = lastItem.action,
+				newStack = [],
+				i;
+
+			for (i = 0; i < this._stack.length; i++) {
+				newStack.push(this._stack[i]);
+
+				if (
+					this._stack[i].module == lastModule
+					&& this._stack[i].action == lastAction
+				) {
+					newStack[newStack.length - 1].parameters = parameters;
+
+					break;
+				}
+			}
+
+			this._stack = newStack;
+		}
 	};
 
 	return new Navi();
