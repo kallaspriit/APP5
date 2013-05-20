508ed139c2c31069395569026a99a92a02188930
Priit Kallas <kallaspriit@gmail.com>
2013-05-20 14:21:40 +0300

    Improved the datepicker widget.

--- app/addons/datepicker/datepicker.js
+++ app/addons/datepicker/datepicker.js
@@ -1,27 +1,83 @@
 define(
-['ResourceManager', 'App', 'Util', 'addons/datepicker/glDatePicker.min'],
+['ResourceManager', 'App', 'Util', 'addons/datepicker/glDatePicker'],
 function(resourceManager, app, util) {
 	'use strict';
 
-	var link = function($scope, $element, $attrs) {
-		resourceManager.loadCss('addons/datepicker/glDatePicker.default.css');
+	var formatDate = function(date) {
+			return (date.getDate() < 10 ? '0' : '') + date.getDate()
+				+ '.' + (date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1)
+				+ '.' + date.getFullYear();
+		},
 
-		$($element).glDatePicker({
-			onClick: function(el, _, date) {
-				var dateStr = (date.getDate() < 10 ? '0' : '') + date.getDate()
-					+ '.' + (date.getMonth() + 1 < 10 ? '0' : '') + (date.getMonth() + 1)
-					+ '.' + date.getFullYear();
+		parseDate = function(date) {
+			var currentDate = new Date(),
+				year = currentDate.getFullYear(),
+				month = currentDate.getMonth(),
+				day = currentDate.getDate(),
+				tokens = date.split('.');
 
-				el.val(dateStr);
-
-				if (!util.isUndefined($attrs.ngModel)) {
-					$scope.$parent[$attrs.ngModel] = dateStr;
-
-					app.validate();
-				}
+			if (tokens.length === 3) {
+				day = parseInt(tokens[0], 10);
+				month = parseInt(tokens[1], 10) - 1;
+				year = parseInt(tokens[2], 10);
 			}
-		});
-	};
+
+			return new Date(year, month, day);
+		},
+
+		link = function($scope, $element, $attrs) {
+			resourceManager.loadCss('addons/datepicker/glDatePicker.default.css');
+
+			var unwatchData = null,
+				datePicker = null,
+				date = new Date();
+
+			if (!util.isUndefined($attrs.ngModel)) {
+				unwatchData = $scope.$watch('model', function(data) {
+					if (util.isString(data)) {
+						date = parseDate(data);
+
+						$element.val(formatDate(date));
+					}
+				});
+			}
+
+			window.setTimeout(function() {
+				datePicker = $($element).glDatePicker({
+					onClick: function(el, _, date) {
+						var dateStr = formatDate(date);
+
+						el.val(dateStr);
+
+						if (!util.isUndefined($attrs.ngModel)) {
+							$scope.model = dateStr;
+
+							app.validate();
+						}
+					},
+					selectedDate: date,
+					zIndex: 10000,
+					/*selectableDateRange: [{
+						from: new Date(1900, 1, 1),
+						to: new Date()
+					}]*/
+					startDate: new Date(1900, 0, 1),
+					endDate: new Date()
+				}).glDatePicker(true);
+
+
+			}, 500); // TODO Animation makes the position calculation fail
+
+			$element.bind('$destroy', function() {
+				if (util.isFunction(unwatchData)) {
+					unwatchData();
+				}
+
+				if (datePicker !== null) {
+					$(datePicker.calendar[0]).remove();
+				}
+			});
+		};
 
 	/**
 	 * Datepicker directive.
@@ -32,7 +88,10 @@ function(resourceManager, app, util) {
 	 */
 	return [function() {
 		return {
-			scope: true,
+			//scope: true,
+			scope: {
+				model: '=ngModel'
+			},
 			restrict: 'E', // EACM
 			transclude: true,
 			replace: true,
