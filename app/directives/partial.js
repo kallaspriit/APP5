3676d269b815e459e2beda37987d616a7d310404
Priit Kallas <kallaspriit@gmail.com>
2014-04-01 11:11:37 +0300

    It is now possible to pass parameters to partials, resolved #3.

--- app/directives/partial.js
+++ app/directives/partial.js
@@ -11,28 +11,38 @@ function(app, resourceManager, util, config) {
 	 * @static
 	 */
 	return ['$compile', function($compile) {
-		var controller = function($scope, $element, $attrs) {
+		var link = function($scope, $element, $attrs) {
 				var module = $attrs.module,
 					activity = $attrs.activity,
+					parameters = {},
 					controllerName = module + '.' + activity,
-					//className = util.convertEntityName(module) + 'Module',
 					moduleCssFilename = 'modules/' + module + '/style/' + module + '-module.css',
 					viewFilename = 'modules/' + module + '/views/' + module + '-' + activity + '.html',
-					cssPrefix = config.cssPrefix;
+					cssPrefix = config.cssPrefix,
+					viewElement,
+					key;
+
+				for (key in $attrs) {
+					if (key.substr(0, 1) === '$') {
+						continue;
+					}
+
+					parameters[key] = $attrs[key];
+				}
 
 				util.when(
 					resourceManager.loadActivity(module, activity),
 					resourceManager.loadView(viewFilename),
 					resourceManager.loadCss(moduleCssFilename)
 				).done(function(activityInstance, viewContent) {
+					activityInstance.setParameters(parameters);
+
 					app.registerController(
 						controllerName,
 						app.getAnnotatedController(activityInstance.onCreate, activityInstance)
 					);
 
-					var viewElement = $(viewContent);
-
-					//$element.html(viewContent);
+					viewElement = $(viewContent);
 					viewElement.attr('ng-controller', controllerName);
 					viewElement.addClass(
 						cssPrefix + 'partial ' + module + '-module ' + activity + '-activity ' +
@@ -41,40 +51,19 @@ function(app, resourceManager, util, config) {
 
 					$element.replaceWith(viewElement);
 
-
 					$compile(viewElement)($scope);
 
 					$scope.$evalAsync(function () {
 						viewElement.removeClass(cssPrefix + 'partial-loading');
 					}.bind(this));
-
-					//app.validate();
-
-					//$compile(viewElement)($scope);
-
-					//app.compile($element)(app.baseScope);
-
-					console.log('loaded partial', activityInstance, viewContent);
 				});
-
-				console.log('controller', module, activity, $attrs);
-
-				//$attrs.$set('ngController', 'test');
-			},
-			link = function($scope, $element, $attrs) {
-				console.log('link', $scope, $element, $attrs);
-
-				//$attrs.$set('ngController', 'test');
 			};
 
 		return {
 			scope: true,
 			restrict: 'E',			// restrict to elements
 			replace: true,			// replace the <partial> tag
-			transclude: 'element',	// transclude the whole element including any directives defined at lower priority
-			controller: controller,
 			link: link,
-			//template: '<div ng-controller>!{{message}}!</div>'
 			template: '<div class="loading-partial"></div>'
 		};
 	}];
